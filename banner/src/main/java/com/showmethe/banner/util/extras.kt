e8b5b2c5eb9df9654e1fd28cd3f4b8a8d9f30f71
ken <c.uvsuv@163.com>
2019-11-28 22:19:16 +0800

    Initial commit

---
+++ banner/src/main/java/com/showmethe/banner/util/extras.kt
@@ -0,0 +1,18 @@
+package com.showmethe.banner.util
+
+import android.content.Context
+
+/**
+ *  com.showmethe.banner.util
+ *  2019/11/28
+ *  21:13
+ */
+fun dip2px(context: Context, dipValue: Float): Int {
+    val scale = context.resources.displayMetrics.density
+    return (dipValue * scale + 0.5f).toInt()
+}
+
+fun px2dip(context: Context, pxValue: Float): Float {
+    val scale = context.resources.displayMetrics.density
+    return (pxValue / scale + 0.5f)
+}
